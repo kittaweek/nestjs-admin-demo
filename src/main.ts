fddad250e15230cecba05b0baf040bcfea7b9eb7
Kittawee Kongpan <kittawee9469@gmail.com>
2024-09-30 20:42:20 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Seagger API endpoint
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Admin Demo')
+    .setDescription('NestJS Admin Demo description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
